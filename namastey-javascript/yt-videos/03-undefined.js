cedd6eb1679308c6af9a56b00590429a5cdb1345
Aviral Goel <avigoel.3888@gmail.com>
2025-04-03 21:34:25 +0530

    03- undefined file added

---
+++ namastey-javascript/yt-videos/03-undefined.js
@@ -0,0 +1,19 @@
+console.log("03 - undefined vs Not defined initiate");
+
+
+/* Eg 1 */
+/*
+console.log(a);     // undefined
+console.log(x);     // not defined
+var a = 7;
+*/
+
+/* Eg 2 */
+/*
+console.log(a);     // Uncaught ReferenceError: Cannot access 'a' before initialization
+console.log(x);     // not defined
+let a = 7;
+*/
+
+// var a = undefined // This will work but is considered as a BAD PRACTICE
+
