21a39ff027fe0e0e70c45898e339a81e503e6a07
Aviral Goel <avigoel.3888@gmail.com>
2025-04-13 12:46:15 +0530

    15 - map, filter, reduce with examples added

--- namastey-javascript/yt-videos/15-map_filter_reduce.js
+++ namastey-javascript/yt-videos/15-map_filter_reduce.js
@@ -4,4 +4,65 @@ console.log("15 - Map, filter and reduce HOF in JS");
 
 All of them are Higher Order Functions and are most commonly used out of all.
 
+Things learned:
+1. map method is used when we want transformation of whole array.
+2. filter is used when we want to filter the arrar to obtain required value.
+3. reduce is used when we want to reduce the array to single value eg (max, min, avg, sum, difference etc).
+4. reduce passes two arguments one function(which includes accumulator and initial value as argument itself) and another initial value of accumulator.
+
 */
+
+// ----------------------------------------------------------------------------------------
+
+// Tricky Eg
+
+const users = [
+    { firstName: "akshay", lastName: "saini", age: 26 },
+    { firstName: "donald", lastName: "trump", age: 75 },
+    { firstName: "elon", lastName: "musk", age: 50 },
+    { firstName: "deepika", lastName: "padukone", age: 26 },
+];
+
+
+// TASK_1: Get list of full name from this users Array ===> [].map()
+const createFullName = (user) => {
+    return user.firstName + " " + user.lastName;
+};
+console.log(users.map(createFullName));
+
+// TASK_2: Get list of users above the age of 40 ===> [].filter()
+console.log(users.filter(user => user.age >= 40));
+
+// TASK_3: Get user name whose age is greater than 40 ===> Chaining of filter and map functions
+const getUsersAbove40 = users.filter((user) => user.age >= 40).map(createFullName);
+console.log(getUsersAbove40);
+
+ 
+// TASK_4: How many users are of a particular age ===> Using normal functions
+const getAgeMap = (users) => {
+    let map = {};
+    users.forEach(user => {
+        map[user.age] ? map[user.age] += 1 : map[user.age] = 1;
+    });
+    return map;
+}
+console.log(getAgeMap(users));
+
+// TASK_5: How many users are of a particular age ===> [].reduce()
+
+// [].reduce((accumulator, currentValue) => {}, initialValueOfAccumulator);
+const getMapReduce = users.reduce((acc, curr) => {
+    console.log(curr);
+    acc[curr.age] ? acc[curr.age] += 1 : acc[curr.age] = 1;
+    console.log(acc);
+    return acc;
+}, {});
+console.log(getMapReduce);
+
+
+// TASK_6: Get first name whose age is greater than 30 ===> [].reduce()
+const getUsersAbove30 = users.reduce((acc, user) => {
+    user.age > 30 ? acc.push(user.firstName) : ""
+    return acc; 
+}, []);
+console.log(getUsersAbove30);
--- namastey-javascript/yt-videos/15-z1-map_usecases.js
+++ namastey-javascript/yt-videos/15-z1-map_usecases.js
@@ -2,23 +2,22 @@ console.log("15 - map use cases");
 
 // [].map() is used to transform an array;
 
-const arr = [5, 1, 3, 6, 2];
-console.log(arr);
+// const arr = [5, 1, 3, 6, 2];
+// console.log(arr);
 
 // Eg: 
 
 // Double:
-const double = function (num) { return num * 2 };
-console.log(arr.map(double));
+// const double = function (num) { return num * 2 };
+// console.log(arr.map(double));
 
 // Triple
-const triple = function (num) { return num * 3 };
-console.log(arr.map(triple));
+// const triple = function (num) { return num * 3 };
+// console.log(arr.map(triple));
 
 // Binary
 // const binary = function (num) { return num.toString(2) };
 // console.log(arr.map(binary));
 // ||
 
-console.log(arr.map(num => num.toString(2)));
- 
+// console.log(arr.map(num => num.toString(2)));
