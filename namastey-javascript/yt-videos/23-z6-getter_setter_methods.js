786cc5362894725fea4dcf9851f92c1a211d861a
Aviral Goel <avigoel.3888@gmail.com>
2025-04-21 23:38:02 +0530

    23 - OOPs completed

--- namastey-javascript/yt-videos/23-z6-getter_setter_methods.js
+++ namastey-javascript/yt-videos/23-z6-getter_setter_methods.js
@@ -1,1 +1,29 @@
 console.log("23 - OOPs - getters and setters in js");
+
+class Employee {
+    constructor(name) {
+        this._name = name;
+    }
+
+    getInfo() {
+        console.log(`Employee ${this._name} is working`);
+    }
+
+    get name() {
+        return this._name
+    };
+
+    set name(newName) {
+        this._name = newName
+    };
+}
+
+const ab = new Employee("Ajay");
+
+ab.getInfo();
+
+console.log(ab.name);
+ab.name = "Ajay Pandey";
+console.log(ab.name);
+
+ab.getInfo(); 
