641c5722d57800ffea6aef259af3b90a46cd2f16
Aviral Goel <avigoel.3888@gmail.com>
2025-05-02 10:34:07 +0530

    Strings - basics and methods added

--- namastey-javascript/yt-videos/strings/01-z1-strings_basics.js
+++ namastey-javascript/yt-videos/strings/01-z1-strings_basics.js
@@ -1,20 +1,66 @@
 console.log("Strings in Js");
 
+/*
 
-// Initializing a String
+Strings are immutable - cannot be changed
 
-let name = "Amit";
-const repoCount = 50;
+*/
 
-// Adding / Concatenating strings
-console.log(name + repoCount); // Old and not a good way to do this
 
-// String Interpolation 
-console.log(`Name: ${name}, and repoCount:${repoCount}`) // Newer way of doing this.
+/* -------       Strings Initialization        ------- */
 
+let name1 = "Amit";
+let name2 = 'This is general String Literal';
+let name3 = `This is
+Template Literal 
+Introduced in ES6
+Can span over multiple lines.`
+
+console.log(name1, "-> ", name2);
+console.log(name3);
+
+/*  -------         Difference between String Literals and Template Literals
+
+| Feature                | String Literal (`'`, `"`)      | Template Literal (`` ` ``)         |
+|------------------------|--------------------------------|------------------------------------|
+| Syntax                 | `'Hello'`, `"Hello"`           | `` `Hello` ``                      |
+| Multiline Support      | ❌ Use `\n` only                | ✅ Native multiline                |
+| Expression Embedding   | ❌ Not supported                | ✅ `${expression}`                 |
+| Easier to Read/Format  | ❌                              | ✅                                 |
+| ES Version             | Available from start           | ES6 and newer                      |
+
+*/
 
 // Another way of initializing a String using String constructor
+let newName = new String('Sample String');
+// console.log(newName, typeof newName);                            // type is Object
 
-let newName = new String('Jatin');
-console.log(newName, typeof newName); // type is Object
+
+// console.log`Hello ${'world'}, lets check template literal`;      // [ 'Hello ', ', lets check template literal' ] world
+
+
+/* -------       Strings Concatenation        ------- */
+// const repoCount = 50;
+// console.log(name1 + repoCount);                                  // Old and not a good way to do this
+
+// String Interpolation 
+// console.log(`Name: ${name1}, and repoCount:${repoCount}`)        // Newer way of doing this.
+
+
+
+
+let str = "Sample String"
+
+/* -------       Strings - Looping over Strings         ------- */
+// for(let i = 0; i< str.length; i++) {
+    // console.log(str[i]);
+// }
+
+
+/* -------       Modifying Strings         ------- */
+// str[2] = "M";                                                    // This doesnot update the strings.
+// console.log(str);                                           
+// console.log(str.replace('m', 'M'));                              // SaMple String
+// console.log(str.replaceAll('S', 's'));                           // sample string
+
 
--- namastey-javascript/yt-videos/strings/02-strings_methods.js
+++ namastey-javascript/yt-videos/strings/02-strings_methods.js
@@ -1,11 +1,17 @@
 console.log("Strings in Js - Methods");
 
 let gameName = new String('PubG');
-
+let str = "Sample String";
 
 /* -------       length        ------- */
-console.log(gameName.length);           // 4
-console.log(gameName.__proto__);        // String object will all string methods 
+// console.log(gameName.length);           // 4
+// console.log(gameName.__proto__);        // String object will all string methods 
+
+
+
+
+/* -------       concat        ------- */
+// console.log(str.concat("! Concatenated Once", "!! And again"));   // Sample String! Concatenated Once!! And again
 
 
 
@@ -13,8 +19,8 @@ console.log(gameName.__proto__);        // String object will all string methods
 /* -------       toLowerCase        ------- */
 // makes complete string in lower case and does not update the original string
 
-gameName = gameName.toLowerCase();
-console.log(gameName);                  // pubg
+// gameName = gameName.toLowerCase();
+// console.log(gameName);                  // pubg
 
 
 
@@ -22,8 +28,8 @@ console.log(gameName);                  // pubg
 /* -------       toUpperCase        ------- */
 // makes complete string in upper case and does not update the original string
 
-gameName = gameName.toUpperCase();
-console.log(gameName);                  // PUBG
+// gameName = gameName.toUpperCase();
+// console.log(gameName);                  // PUBG
 
 
 
@@ -31,8 +37,8 @@ console.log(gameName);                  // PUBG
 /* -------       charAt        ------- */
 // using charAt we can check which character at present at which index
 
-console.log(gameName.charAt(2));        // B
-console.log(gameName.charAt(12));       // empty - since at index 12 nothing is present
+// console.log(gameName.charAt(2));        // B
+// console.log(gameName.charAt(12));       // empty - since at index 12 nothing is present
 
 
 
@@ -40,5 +46,70 @@ console.log(gameName.charAt(12));       // empty - since at index 12 nothing is 
 /* -------       indexOf        ------- */
 // check the character at index
 
-console.log(gameName.indexOf('b'));     // -1 (since 'b' is not present)
-console.log(gameName.indexOf('B'));     // 2 (B is present at 2nd index only)
+// console.log(gameName.indexOf('b'));     // -1 (since 'b' is not present)
+// console.log(gameName.indexOf('B'));     // 2 (B is present at 2nd index only)
+
+
+
+let sportsName = new String("Foot-ball");
+console.log(sportsName);
+
+/* -------       substring()        ------- */
+// Returns the substring at the specified location within a String object.
+// substring(startIndex, endIndex) -> startIndex is included but endIndex is excluded.
+// if endIndex is not provided -> it returns the sub string from startIndex till the end
+// we can give -ve values in index but, substring will not obey and will consider to begin from 0 index.
+
+// console.log(sportsName.substring(0, 4));    // Foot 
+// console.log(sportsName.substring(3));       // t-ball
+
+
+
+
+/* -------       slice()        ------- */
+
+// slice(start, end) the original string from startIndex to endIndex (start - included && end - not included)
+// if endIndex is not provided -> it returns the sub string from startIndex till the end
+// IMP: it takes -ve index as well and in that case, starts from last
+
+// console.log(sportsName.slice(2), sportsName);    // ot, String {'Foot-ball'}
+// console.log(sportsName.slice(-6, 8));            // t-bal
+
+
+
+
+/* -------       trim()        ------- */
+// removes spaces from start and end of string 
+// other methods include [trimStart, trimEnd]
+
+const spaceString = new String("    first    second   ");
+console.log(spaceString);                       //     first    second   
+console.log(spaceString.trim());                // first    second
+
+
+
+
+/* -------       replace() & replaceAll()      ------- */
+// (method) String.replace(searchValue: string | RegExp, replaceValue: string): string (+3 overloads)
+// Replaces text in a string, using a regular expression or search string.
+
+// replace -> replaces first instance of that char or word
+// replaceAll -> replaces all instances of that char or word
+
+console.log(sportsName.replace('-', '_'));      // Foot_ball (earlier was Foot-ball)
+console.log(sportsName.replaceAll('o', 'O'));      // FOOt-ball (earlier was Foot-ball)
+
+
+
+
+
+
+/* -------       includes()        ------- */
+console.log(sportsName.includes("b"));      // true
+console.log(sportsName.includes("z"));      // false
+
+
+
+/* -------       split()        ------- */
+let splitName1 = "one-two-three-four";
+console.log(splitName1.split('-'));         // ['one', 'two', 'three', 'four']
