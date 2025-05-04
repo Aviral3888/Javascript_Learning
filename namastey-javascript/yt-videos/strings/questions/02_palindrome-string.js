1bbe061df6e939f7a4faff2459e24f786a54ea65
Aviral Goel <avigoel.3888@gmail.com>
2025-05-04 18:08:33 +0530

    Palindrome solution added

--- namastey-javascript/yt-videos/strings/questions/02_palindrome-string.js
+++ namastey-javascript/yt-videos/strings/questions/02_palindrome-string.js
@@ -25,7 +25,7 @@ function checkPalindrome(str) {
     return true
 }
 
-console.log(checkPalindrome("12321"));
+// console.log(checkPalindrome("12321"));
 
 
 /*
@@ -35,4 +35,60 @@ start = 0     end = 4   1   1 => true
 start = 1     end = 3   2   2 => true
 start = 2     end = 2   3   3 => true
 
+*/
+
+/* For Solution 1:
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+
+⏱ Time Complexity:
+	•	The loop runs only half the length of the string → n / 2
+	•	Comparisons are constant-time (O(1) per iteration)
+
+🔸 Total Time Complexity: O(n) (where n is the length of the string)
+
+🧠 Space Complexity:
+	•	Creates a new substring → O(n) for the sliced portion
+	•	Final returned string is also new → O(n + 3) (practically still O(n))
+*/
+
+
+
+
+
+
+// Solution 2: Using Js String methods
+
+function checkPalindrome(str) {
+
+    let reverseStr = str.split("").reverse().join("");
+
+    if (str == reverseStr){
+        return true;
+    }
+    return false;
+}
+
+console.log(checkPalindrome("12321"));
+
+
+/* For Solution 1:
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+
+⏱ Time Complexity:
+	1.	split("") → O(n)
+	2.	reverse() → O(n)
+	3.	join("") → O(n)
+
+➡️ Total Time Complexity = O(n)
+
+💾 Space Complexity:
+	•	You’re creating a new array and a new string (from join), each of size n.
+
+➡️ Space Complexity = O(n)
 */
