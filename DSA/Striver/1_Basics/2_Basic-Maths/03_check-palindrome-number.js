21ed13841ca7a19858485ce2f1345b6458304fe6
Aviral Goel <avigoel.3888@gmail.com>
2025-05-22 22:54:02 +0530

    Palindrome Solution added

--- DSA/Striver/1_Basics/2_Basic-Maths/03_check-palindrome-number.js
+++ DSA/Striver/1_Basics/2_Basic-Maths/03_check-palindrome-number.js
@@ -1,2 +1,36 @@
 /*
+Check if a number is Palindrome or Not
+
+Problem Statement: Given an integer N, return true if it is a palindrome else return false.
+A palindrome is a number that reads the same backward as forward. For example, 121, 1331, and 4554 are palindromes because they remain the same when their digits are reversed.
+
+
+Example 1:
+Input:N = 4554
+               
+Output:Palindrome Number
+Explanation: The reverse of 4554 is 4554 and therefore it is palindrome number
+        
+Example 2:
+Input:N = 7789
+
+Output: Not Palindrome
+Explanation: The reverse of number 7789 is 9877 and therefore it is not palindrome
 */
+
+function checkPalindrome(num) {
+    let rev = 0;
+    let orig = num;
+    if (num < 0) return "Not Palindrome";
+
+    while (num !=0) {
+        let ld = num % 10;
+        rev = rev * 10 + ld;
+        num = Math.floor(num / 10);
+    }
+
+    if (rev == orig) return "Palindrome Number";
+    else return "Not Palindrome";
+}
+
+console.log(checkPalindrome(-87));
