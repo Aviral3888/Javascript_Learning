b394b9cbb183785afec2c404e3d4fcd5fa40b7c6
Aviral Goel <avigoel.3888@gmail.com>
2025-05-28 21:16:11 +0530

    GCD solution added - Normal and Euclidean Algo.

--- DSA/Striver/1_Basics/2_Basic-Maths/04_find-GCD-or-HCF.js
+++ DSA/Striver/1_Basics/2_Basic-Maths/04_find-GCD-or-HCF.js
@@ -1,5 +1,5 @@
 /*
-Check if a number is Palindrome or Not
+Find GCD of two numbers
 
 Problem Statement: Given two integers N1 and N2, find their greatest common divisor.
 The Greatest Common Divisor of any two integers is the largest number that divides both integers.
@@ -16,13 +16,12 @@ Common Factors: 1, 3 out of which 3 is the greatest hence it is the GCD.
 Example 2:
 Input:N1 = 20, N2 = 15
 
-
 Output: 5
 Explanation:Factors of 20: 1, 2, 4, 5
 Factors of 15: 1, 3, 5
 Common Factors: 1, 5 out of which 5 is the greatest hence it is the GCD.
-*/
 
+*/
 
 // SOLUTION 1: WORKS but not optimal
 
@@ -56,16 +55,59 @@ function findGCD(num1, num2) {
 
 
     console.log(max, min)
-    for (let i = min.length -1 ; i >= 0; i--) {
-        for (let j = max.length -1; j >= 0; j--) {
+    for (let i = min.length - 1; i >= 0; i--) {
+        for (let j = max.length - 1; j >= 0; j--) {
             if (max[i] == min[j]) {
                 return max[i];
             }
         }
     }
 }
-console.log(findGCD(9, 99));
+// console.log(findGCD(9, 99));
 
 
 
 // SOLUTION 2: Efficient Approach
+
+/*
+Step by step:
+
+Loop from MIN (num1, num2) till 1, 
+The number whose remainder with both is 0 is the solution
+*/
+
+function findGCD_2(num1, num2) {
+    let gcd = 0;
+    for (let i = Math.min(num1, num2); i >= 1; i--) {
+        console.log(i);
+        if (num1 % i == 0 && num2 % i == 0) {
+            gcd = i;
+            break;
+        }
+    };
+    return gcd;
+}
+
+// console.log(findGCD_2(9, 12));
+
+/*
+Easy solution - 
+Time Complexity O(n)
+*/ 
+
+
+
+// BUT this can further be reduced using Euclidean Algorithm
+
+function findGCD_Euclidean(a, b){
+    while (a > 0 && b > 0) {
+        if (a > b) a = a % b
+        else b = b % a
+    }
+    if (a == 0) return b;
+    else return a;
+}
+
+console.log(findGCD_Euclidean(9, 12));
+
+// T.C = O(log (min(m, n)))
