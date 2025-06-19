98c07ec363dc16319de4d0cacd4ef3a5601c83b9
Aviral Goel <avigoel.3888@gmail.com>
2025-06-19 21:27:00 +0530

    Arrays - subarray problem added

--- 2_DSA/1_Scaler/2_Questions/01_Lectures/04_02_Subarrays.js
+++ 2_DSA/1_Scaler/2_Questions/01_Lectures/04_02_Subarrays.js
@@ -1,3 +1,4 @@
+// Problem 1: 
 // Print all subarray elements
 
 let arr_1 = [3, -2, 6, 2, 4];
@@ -22,4 +23,29 @@ function printAllSubarrays(arr) {
     console.log("SUM:", sum);
 }
 
-console.log(printAllSubarrays(arr_1));
+// console.log(printAllSubarrays(arr_1));
+
+
+// Problem 2:
+// Given an array of N elements, and a, b reverse the subarray from a - b
+
+/*
+Eg: Input arr = [1,2,3,4,5,6,7], a = 2, b = 5;
+Output = [1,2,6,5,4,3,7]
+*/
+
+let arr_2 = [1, 2, 3, 4, 5, 6, 7], a = 2, b = 5;
+function reverseSubArray(arr, a, b) {
+    let start = a;
+    let end = b;
+
+    while(start < end) {
+        arr[start] = arr[start] ^ arr[end];
+        arr[end] = arr[start] ^ arr[end];
+        arr[start] = arr[start] ^ arr[end];
+        start++;
+        end--;
+    }
+    return arr;
+}
+console.log(reverseSubArray(arr_2, a, b))
