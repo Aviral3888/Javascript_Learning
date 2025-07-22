beb7c39af63dca412bf1380a51b3e64b81c7726c
Aviral Goel <avigoel.3888@gmail.com>
2025-07-22 21:49:04 +0530

    DSA: Subarray added

--- 2_DSA/1_Scaler/2_Questions/02_Topicwise/04_Arrays/00_Revision/01_Array_01.js
+++ 2_DSA/1_Scaler/2_Questions/02_Topicwise/04_Arrays/00_Revision/01_Array_01.js
@@ -66,8 +66,85 @@ console.log(revereseArray(arr_3));
 // ----------------------------------------------------------------------------------------------------
 
 
-// Problem 1: 
+// Problem 3: 
 // Print all subarray elements
-
+/*
 let arr_1 = [3, -2, 6, 2, 4];
-function printAllSubarrays(arr) {}
+function printAllSubarrays(arr) {
+    let N = arr.length;
+
+    for (let i = 0; i < N; i++) {
+        for (let j = i; j < N; j++) {
+            console.log(`[${i}-${j}]`)
+            for (let k = i; k <= j; k++) {
+                console.log(arr[k]);
+            }
+        }
+    }
+}
+
+console.log(printAllSubarrays(arr_1))
+*/
+
+
+// ----------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------
+
+
+// Problem 4: 
+// Print all subarray elements
+/*
+let arr_1 = [3, -2, 6, 4, 2];
+function printAllSubarraysSum(arr) {
+    let N = arr.length;
+    let sumOfSubarrays = 0;
+    for (let i = 0; i < N; i++) {
+        for (let j = i; j < N; j++) {
+            console.log(`[${i}-${j}]`)
+            let sumOfAllSubarrays = 0;
+            for (let k = i; k <= j; k++) {
+                // console.log(arr[k]);
+                sumOfSubarrays += arr[k];
+                sumOfAllSubarrays += arr[k];
+            }
+            console.log("Sum of all Subarrays:", sumOfAllSubarrays)
+        }
+    }
+    console.log("Sum of Subarrays:", sumOfSubarrays);
+}
+
+console.log(printAllSubarraysSum(arr_1))
+*/
+
+
+
+// ----------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------
+
+
+
+// Problem 5:
+// Given an array of N elements, and a, b reverse the subarray from a - b
+
+/*
+Eg: Input arr = [1,2,3,4,5,6,7], a = 2, b = 5;
+Output = [1,2,6,5,4,3,7]
+*/
+/*
+let arr_2 = [1, 2, 3, 4, 5, 6, 7], a = 2, b = 5;
+
+function reverseSubArray(arr, a, b) {
+    while(a < b) {
+        let temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        a++;
+        b--;
+    }
+    return arr;
+}
+
+console.log(reverseSubArray(arr_2, a, b))
+*/
