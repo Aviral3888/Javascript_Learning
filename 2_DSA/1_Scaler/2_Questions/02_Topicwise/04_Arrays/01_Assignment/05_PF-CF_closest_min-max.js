57efeab2b03f9325991e99806033c712d15c65f1
Aviral Goel <avigoel.3888@gmail.com>
2025-08-12 22:28:20 +0530

    DSA: Array: Closest Min Max: added

--- 2_DSA/1_Scaler/2_Questions/02_Topicwise/04_Arrays/01_Assignment/05_PF-CF_closest_min-max.js
+++ 2_DSA/1_Scaler/2_Questions/02_Topicwise/04_Arrays/01_Assignment/05_PF-CF_closest_min-max.js
@@ -32,63 +32,110 @@ Explanation 2:
 Take the last 3 elements of the array.
 */
 
-// var A = [2, 6, 1, 6, 9];
-let A = [834, 563, 606, 221, 165];
 
-function closestMinMax(A) {
-    if ([1, 2].includes(A.length)) return 1;
+
+// SOLUTION SELF: 
+
+// var A = [2, 6, 1, 6, 9];
+
+// let A = [834, 563, 606, 221, 165];
+
+// function closestMinMax(A) {
+//     if ([1, 2].includes(A.length)) return 1;
+//     let min = Infinity;
+//     let max = -Infinity;
+//     for (let i = 0; i < A.length; i++) {
+//         min = Math.min(min, A[i]);
+//         max = Math.max(max, A[i]);
+//     }
+//     console.log("Min:", min, "Max:", max);
+
+//     if (min == max) { return 1 };
+
+//     let count = 0;
+//     let ans = A.length;
+//     let set = "none";
+
+//     for (let i = 0; i < A.length; i++) {
+//         if (A[i] == min) {
+
+//             if (set == "none") {
+//                 count++;
+//                 set = "min"
+//             }
+//             else if (set == "min") {
+//                 count = 1;
+//             }
+//             else {
+//                 count++;
+//                 ans = Math.min(ans, count);
+//                 count = 1;
+//                 set = "min";
+//             }
+//         }
+//         else if (A[i] == max) {
+//             if (set == "none") {
+//                 count++;
+//                 set = "max"
+//             }
+//             else if (set == "max") {
+//                 count = 1;
+//             }
+//             else {
+//                 count++;
+//                 ans = Math.min(ans, count);
+//                 count = 1;
+//                 set = "max";
+//             }
+//         }
+//         else {
+//             if (set != "none") { count++ };
+//         }
+//         console.log(i, A[i], "Set:", set, "Count:", count, "Ans:", ans);
+//     }
+//     return ans;
+// }
+
+// console.log(closestMinMax(A));
+
+
+
+
+// ---------- SOLUTION 2 - Brute Force
+
+let A = [2, 2, 6, 4, 5, 1, 5, 2, 6, 4, 1];
+
+function getClosestMinMax(A) {
+
+    let N = A.length;
+
     let min = Infinity;
     let max = -Infinity;
-    for (let i = 0; i < A.length; i++) {
+    for (let i = 0; i < N; i++) {
         min = Math.min(min, A[i]);
         max = Math.max(max, A[i]);
     }
-    console.log("Min:", min, "Max:", max);
-
-    if (min == max) { return 1 };
-
-    let count = 0;
-    let ans = A.length;
-    let set = "none";
-
-    for (let i = 0; i < A.length; i++) {
+    
+    let ans = N - 1;
+    for (let i = 0; i < N; i++) {
         if (A[i] == min) {
-
-            if (set == "none") {
-                count++;
-                set = "min"
-            }
-            else if (set == "min") {
-                count = 1;
-            }
-            else {
-                count++;
-                ans = Math.min(ans, count);
-                count = 1;
-                set = "min";
+            for (let j = i + 1; j < N; j++) {
+                if (A[j] == max) {
+                    console.log(i, j)
+                    ans = Math.min(ans, (j - i + 1));
+                }
             }
         }
         else if (A[i] == max) {
-            if (set == "none") {
-                count++;
-                set = "max"
-            }
-            else if (set == "max") {
-                count = 1;
-            }
-            else {
-                count++;
-                ans = Math.min(ans, count);
-                count = 1;
-                set = "max";
+            for (let j = i + 1; j < N; j++) {
+                if (A[j] == min) {
+                    console.log(i, j)
+                    ans = Math.min(ans, (j - i + 1));
+                }
             }
         }
-        else {
-            if (set != "none") { count++ };
-        }
-        console.log(i, A[i], "Set:", set, "Count:", count, "Ans:", ans);
     }
     return ans;
 }
 
-console.log(closestMinMax(A));
+console.log(getClosestMinMax(A));
